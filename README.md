48ae1b921bed74d7fb98fd87f60ee62e71b9c057
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2021-08-09 21:30:18 -0500

    Reto de las figuras geométricas completado al integrar JS y JTML

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ Ejercicios para practicar JS
 ## Taller #1 figuras geométricas
 
 - Primer paso: definir las fórmulas.
-- Segudo paso: implementar las formulas de JS.
+- Segudo paso: implementar las fórmulas de JS.
+- tercer paso: crear funciones.
+- cuarto paso: integrar JS con HTML.
+
+## Taller #2 Porcentajes y descuentos
+
+- Primer paso: definir las fórmulas.
+- Segudo paso: implementar las fórmulas de JS.
 - tercer paso: crear funciones.
 - cuarto paso: integrar JS con HTML.
--- figuras.js
+++ figuras.js
@@ -6,43 +6,112 @@ console.log("Hello, world!");
 console.group("Cuadrado");
 
 function perimetroCuadrado(ladoCuadrado) {
-    return ladoCuadrado * 4;
+    return Number(ladoCuadrado) * 4;
 }  
 //console.log("El perimetro del cuadrado mide: "+ perimetroCuadrado + " cm^2");
 
 function areaCuadrado(lado) {
-    return lado * lado;
+    return Number(lado) * lado;
 }
 
 console.groupEnd();
 
 //Código del triangulo
-console.group("Triangulo")
+console.group("Triangulo");
 
-function perimetroTriangulo(lado1, lado2, base) {
-    return lado1 + lado2 + base;
+function perimetroTrianguloCal(lado1, lado2, base) {
+    return Number(lado1) + Number(lado2) + Number(base);
 }
 
-function areaTriangulo(base, altura) {
-    return base * altura / 2;
+function areaTrianguloCal(base, altura) {
+    return Number(base) * Number(altura) / 2;
 }
 
 console.groupEnd();
 
+// Triángulo Isosceles
+console.group("Triangulo Isosceles");
+
+function alturaIsoscelesCal(ladoA, base) {
+    const altura = Math.sqrt((ladoA * ladoA) - ((base * base) / 4));
+    return altura;
+}
 //Circulo
 
 console.group("Circulo");
 
 function diametroCirculo(radio) {
-    return radio * 2;
+    return Number(radio) * 2;
 }
 
-function perimetroCirculo(radio) {
-    return diametro(radio) * Math.PI;
+function perimetroCirculoCal(radio) {
+    return diametroCirculo(Number(radio)) * Math.PI;
 }
 
-function areaCirculo(radio) {
-    return Math.PI * radio * radio;
+function areaCirculoCal(radio) {
+    return Math.PI * Number(radio) * Number(radio);
 }
 
 console.groupEnd();
+
+// Funciones para los botones
+
+//Cuadrado  Button
+function calcularPerimetroCuadrado() {
+    const lado_del_cuadrado = document.getElementById("inputCuadrado");
+    const resultado = perimetroCuadrado(Number(lado_del_cuadrado.value));
+    alert(resultado);
+}
+
+function calcularAreaCuadrado() {
+    const lado_del_cuadrado = document.getElementById("inputCuadrado");
+    const resultado = areaCuadrado(Number(lado_del_cuadrado.value));
+    alert(resultado);
+}
+
+//Triángulo  Button
+function perimetroTriangulo() {
+    const lado1 = document.getElementById("inputLado1");
+    const lado2 = document.getElementById("inputLado2");
+    const lado3 = document.getElementById("inputLado3");
+
+    const perimetro = perimetroTrianguloCal(Number(lado1.value), Number(lado2.value), Number(lado3.value));
+    alert(perimetro);
+}
+
+function areaTriangulo() {
+    const lado1 = document.getElementById("inputLado1");
+    const lado2 = document.getElementById("inputLado2");
+    const lado3 = document.getElementById("inputLado3");
+
+    const area = areaTrianguloCal(Number(lado1.value), Number(lado2.value), Number(lado3.value));
+    alert(area);
+}
+
+//Circulo Button
+function perimetroCirculo() {
+    const radio = document.getElementById("inputRadio");
+
+    const perimetro = perimetroCirculoCal(Number(radio.value));
+    alert(perimetro);
+}
+
+function areaCirculo() {
+    const radio = document.getElementById("inputRadio");
+
+    const area = areaCirculoCal(Number(radio.value));
+    alert(area);
+}
+
+//Triangulo Isosceles Button
+function alturaIsosceles() {
+    const ladoA = document.getElementById("inputLadoIsosceles");
+    const base = document.getElementById("inputBaseIsosceles");
+
+    if (base.value > (ladoA.value * 2)) {
+        alert("Ese triángulo no existe, si no preguntaselo a pitágoras");
+        return;
+    }
+    const altura = alturaIsoscelesCal(Number(ladoA.value), Number(base.value));
+    alert(altura);
+}
